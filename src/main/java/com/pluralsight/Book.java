9b8d459ea00045ed3c65a7c6c566699ac29d85e6
Yigit <yigistaff@gmail.com>
2024-10-10 22:44:06 -0400

    updated

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -13,24 +13,28 @@ public class Book {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
-        this.isCheckedOut = false; // right now default
-        this.checkedOutTo = "";    // right now default
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
     }
 
 
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
     }
 
-    public String getIsbn() {
+    public String getIsbn()
+    {
         return isbn;
     }
 
-    public void setIsbn(String isbn) {
+    public void setIsbn(String isbn)
+    {
         this.isbn = isbn;
     }
 
@@ -38,15 +42,18 @@ public class Book {
         return title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title)
+    {
         this.title = title;
     }
 
-    public boolean isCheckedOut() {
+    public boolean isCheckedOut()
+    {
         return isCheckedOut;
     }
 
-    public String getCheckedOutTo() {
+    public String getCheckedOutTo()
+    {
         return checkedOutTo;
     }
 
